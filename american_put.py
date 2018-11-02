ab34b1d445a1bb010d6abd229cee6450fa06ac14
David S. Li <ds.li@outlook.com>
2018-11-01 22:45:32 -0400

    Rearranging things, factoring in option price and recursion

--- american_put.py
+++ american_put.py
@@ -3,30 +3,34 @@
 
 from math import exp, sqrt
 
-# FILE INPUT HERE
+filename = "hello.txt"
+file = open(filename, "r")
+lines = []
+for line in file:
+    lines.append(line)
 
-s = ""
-vars = s.split('\t')
+for line in lines:
+    vars = line.split('\t')
 
-# vars should have 6 elements, if not, make an exception here
-if len(vars) != 6:
-    raise SystemExit # maybe declare your own exception here?
+    # vars should have 6 elements, if not, make an exception here
+    if len(vars) != 6:
+        raise SystemExit # maybe declare your own exception here?
 
-r = float(vars[0]) #
-total_t = float(vars[1]) #
-n = float(vars[2]) #
-sigma = float(vars[3]) # volatility
-s_0 = float(vars[4]) # original stock price
-k = float(vars[5]) # strike price, abbreviation for S_{0}
+    r = float(vars[0]) #
+    total_t = float(vars[1]) #
+    n = float(vars[2]) #
+    sigma = float(vars[3]) # volatility
+    s_0 = float(vars[4]) # original stock price
+    k = float(vars[5]) # strike price, abbreviation for S_{0}
 
-t = total_t / n # delta t
+    t = total_t / n # delta t
 
-# calculate 'u' and 'p'
-# u = e^{sigma * sqrt{t}}
-u = exp(sigma * sqrt(t))
-d = 1 / u
+    # calculate 'u' and 'p'
+    # u = e^{sigma * sqrt{t}}
+    u = exp(sigma * sqrt(t))
+    d = 1 / u
 
-p = (exp(r * t) - d) / (u - d)
+    p = (exp(r * t) - d) / (u - d)
 
 # recursive function for branching
 def branch(price, dir): # price is float, dir is boolean where true represents up and false represents down
@@ -43,14 +47,25 @@ class BinaryTree():
 
 class BinaryTreeNode():
     def  __init__(self, time, parent, dir): # factor in direction from old branch method when creating new nodes?  Pass in parent as a reference?
+        # Determining Stock Price
+        if dir < 0:
+            self.stock_price = self.parent.stock_price * d
+        elif dir > 0:
+            self.stock_price = self.parent.stock_price * u
+        else:
+            self.stock_price = s_0
+
+        #
         if time < n: # Branch and create two children
             self.left = BinaryTreeNode(time + t, self, -1)
             self.right = BinaryTreeNode(time + t, self, 1)
+            self.f = exp(-r * t) * (p * self.left.f + (1 - p) * self.right.f) # Fix 't', if it is supposed to represent the TOTAL time
         else: # LEAF NODE CASE
             self.left = None
             self.right = None
-        self.parent = None # FIX LATER
-        self.stock_price = s_0 * exp(r * time)
-        f_u = self.parent.stock_price * u
-        f_d = self.parent.stock_price * d
-        self.f = exp(-r * t) * (p * f_u + (1 - p) * f_d) # Fix 't', if it is supposed to represent the TOTAL time
+            self.f = self.stock_price - k
+
+        self.parent = parent
+
+        # option price = stock price - strike price
+        #self.f = exp(-r * t) * (p * f_u + (1 - p) * f_d) # Fix 't', if it is supposed to represent the TOTAL time
