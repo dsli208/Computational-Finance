7ce2688c274f1c12f06a94e2f9bbcc90bf633360
dsli208 <ds.li@outlook.com>
2018-12-10 17:24:41 -0500

    Sort of finished HW4 code file

--- HW4/cse390_hw4.py
+++ HW4/cse390_hw4.py
@@ -1,9 +1,11 @@
 import pandas as p
 import statsmodels.tsa.stattools as sm
 import statsmodels.graphics.tsaplots as s
+import statsmodels.regression.linear_model as lm
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.core.multiarray import ndarray
+from sklearn import datasets, linear_model
 
 def mean(list):
     return sum(list) / len(list)
@@ -36,19 +38,29 @@ def calc_acf(data):
         series_1 = p.Series(data_seg1["5Y"])
         series_2 = p.Series(data_seg2["5Y"])
         cov_i = series_1.cov(series_2)
-        acf.append(cov_i)
+        acf.append(cov_i / variance)
 
     return acf
 
 def calc_pacf(data, lag):
-    data_seg_1 = data[:lag]
+    data_seg_1 = data.loc[:, ["5Y"]]
+    series = p.Series(data_seg_1["5Y"])
+    regr = linear_model.LinearRegression()
+
+
     # data_seg1 = data.loc[:, ["5Y"]]
     data_seg_2 = data[lag:]
     # data_seg2 = data.loc[i:, ["5Y"]]
     pacf = sm.pacf(data, nlags=lag, method='ywunbiased')
+    #pacf = []
+    #for k in range(1, 11):
+        #pacf.append(regr.predict(data_seg_1))
+        #pacf.append(lm.yule_walker(p.Series.ravel(series))[0][-1])
     return pacf
 
-data = p.read_csv('bond-returns.csv', sep = "|")
+src = 'bond-returns.csv'
+src = input("Please enter the file: ")
+data = p.read_csv(src, sep = "|")
 
 #print(data) # MUST GIVE COLUMN HEADERS in CSV FILE BEFORE WE DO THIS
 five_year_data = data[["5Y"]]
@@ -56,10 +68,11 @@ print(five_year_data) # how do we get this to work as a 1D array?
 #five_year_data_1d = p.Series.ravel(five_year_data)
 #print(five_year_data_1d)
 acf = calc_acf(five_year_data)
+print(acf[0])
 print("ACF is", acf)
-plt.xlim(1, 10)
+# plt.xlim(1, 10)
 acf_plt = s.plot_acf(acf)
-# plt.show()
 pacf = calc_pacf(five_year_data, 10)
+print(pacf)
 pacf_plt = s.plot_pacf(pacf)
 plt.show()
